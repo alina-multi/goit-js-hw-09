427d3c020f98e3cfe509b09dd471896c3ffb9d62
Alina Akseninko <79420032+alina-multi@users.noreply.github.com>
2022-08-03 21:20:40 +0100

    color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,42 @@
+const body = document.querySelector ("body")
+const btnStart = document.querySelector("[data-start]")
+const btnStop = document.querySelector("[data-stop]")
 
+const PROMPT_DELAY = 700;
+let interval_ID = null;
+
+
+
+btnStart.addEventListener("click", onBtnStartClick)
+btnStop.addEventListener("click", onBtnStopClick)
+
+function onBtnStartClick(e) {
+   
+    interval_ID = setInterval(colorSwitcher, PROMPT_DELAY)
+    btnStart.disabled = "true"
+}
+
+function onBtnStopClick(e) {
+
+    btnStart.removeAttribute("disabled")
+    clearInterval(interval_ID);
+    const bodyCurrentColor = body.getAttribute("style")
+    // console.log(bodyCurrentColor)
+    const colorContent = document.createElement("p")
+    colorContent.textContent = `You caught the color > ${bodyCurrentColor}`
+    console.log(colorContent)
+    btnStart.before(colorContent)
+
+
+}
+
+function colorSwitcher() { 
+
+    body.style.background = getColor()
+    
+}
+ 
+
+const getColor  = function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
